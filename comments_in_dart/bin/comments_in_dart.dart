45694fc685be9c4e32f8706de0f20645b1b53289
SetyadiD <setyadi.darmawan0430@gmail.com>
2025-10-15 09:28:04 +0700

    comments in dart

---
+++ comments_in_dart/bin/comments_in_dart.dart
@@ -0,0 +1,12 @@
+import 'package:comments_in_dart/comments_in_dart.dart' as comments_in_dart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${comments_in_dart.calculate()}!');
+}
+
+// This is a single-line comment
+/* 
+This is 
+a multi-line comment 
+*/
+/// This is a documentation comment
